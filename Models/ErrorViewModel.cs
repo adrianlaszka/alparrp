8e1f81ed08eeb0fc8569eea6acea5362912c6e70
Adrián <adrian.laszka90@gmail.com>
2024-11-17 14:52:44 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AlparRP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
