0f1f8eb57f4baeff355ee8ede2eaa116f6fb4daa
Adrián <adrian.laszka90@gmail.com>
2024-11-19 14:38:12 +0100

    AppDbContext added

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlparRP.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+        public DbSet<DiscordUser> DiscordUsers { get; set; }
+        public DbSet<UserActivityLog> UserActivityLogs { get; set; }
+    }
+}
